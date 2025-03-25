b4de7f4aaf192b2ccfa729dbee4d1c5211a03611
abrar <abrar.farhad@m2amedia.tv>
2025-03-25 14:53:51 +0000

    challenge 3 embargo date

--- interview/order/views.py
+++ interview/order/views.py
@@ -5,11 +5,30 @@ from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
 # Create your views here.
+
+
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderListApiView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        start = self.request.query_params.get("start")
+        embargo = self.request.query_params.get("embargo")
+
+        if start and embargo:
+            queryset = queryset.filter(
+                start_date__gte=start
+            ).filter(embargo_date__lte=embargo)
+
+        return queryset
